5ebd3d86bada98e67002baead19cf7580346d16c
Joe Kim <joesm.genie@gmail.com>
2025-06-09 15:23:30 +0700

    config swagger

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('User Service API')
+    .setDescription('API documentation for the User Service')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
