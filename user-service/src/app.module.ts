1bc73564904c3b6426d978ff752c381435bf2bc4
Joe Kim <joesm.genie@gmail.com>
2025-06-09 21:03:11 +0700

    add config service to app module

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { HealthModule } from './modules/health/health.module';
 import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, HealthModule, DbModule],
+  imports: [UsersModule, HealthModule, DbModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
