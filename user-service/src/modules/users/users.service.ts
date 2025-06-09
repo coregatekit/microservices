6268d02af0c97be4c5505badd529028925b52ece
Joe Kim <joesm.genie@gmail.com>
2025-06-09 21:14:05 +0700

    inject db to user service

--- user-service/src/modules/users/users.service.ts
+++ user-service/src/modules/users/users.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import * as schema from '../../db/drizzle/schema';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './users';
 import { UserResponse } from './users.interface';
+import { DrizzleAsyncProvider } from 'src/db/db.provider';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @Inject(DrizzleAsyncProvider)
+    private readonly db: NodePgDatabase<typeof schema>,
+  ) {}
+
   createUser(createUserDto: CreateUserDto): Promise<UserResponse> {
     const userResponse: UserResponse = {
       id: '1',
