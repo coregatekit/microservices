2a90112e89b0f812a54efd0752d3549483c07a7b
Joe Kim <joesm.genie@gmail.com>
2025-06-09 20:20:14 +0700

    set validation pipe

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = new DocumentBuilder()
     .setTitle('User Service API')
