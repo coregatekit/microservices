caa03b893e3d1217d0dc5cce4f7c87b33d23d866
Joe Kim <joesm.genie@gmail.com>
2025-06-09 21:02:32 +0700

    add db module

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { HealthModule } from './modules/health/health.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [UsersModule, HealthModule],
+  imports: [UsersModule, HealthModule, DbModule],
   controllers: [],
   providers: [],
 })
