592d9036db6f77943090c9aca07fcf2a46e1b177
Joe Kim <joesm.genie@gmail.com>
2025-06-10 22:28:25 +0700

    inject db to auth service

--- user-service/src/modules/auth/auth.service.spec.ts
+++ user-service/src/modules/auth/auth.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { DrizzleAsyncProvider } from '../../db/db.provider';
+
+const mockDb = {};
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: DrizzleAsyncProvider,
+          useValue: mockDb,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- user-service/src/modules/auth/auth.service.ts
+++ user-service/src/modules/auth/auth.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { DrizzleAsyncProvider } from '../../db/db.provider';
+import * as schema from '../../db/drizzle/schema';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(DrizzleAsyncProvider)
+    private readonly db: NodePgDatabase<typeof schema>,
+  ) {}
+}
