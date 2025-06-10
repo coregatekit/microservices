e56a94ed0c9d8704ea2d246ebac37b2f29a4efaf
Joe Kim <joesm.genie@gmail.com>
2025-06-10 22:10:15 +0700

    create module auth

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -3,9 +3,16 @@ import { UsersModule } from './modules/users/users.module';
 import { HealthModule } from './modules/health/health.module';
 import { DbModule } from './db/db.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, HealthModule, DbModule, ConfigModule.forRoot()],
+  imports: [
+    UsersModule,
+    HealthModule,
+    DbModule,
+    ConfigModule.forRoot(),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
