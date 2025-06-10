74b834a5812c595b809fee8ce6b8b12078824c53
Joe Kim <joesm.genie@gmail.com>
2025-06-10 22:20:35 +0700

    create route login with return mock access token

--- user-service/src/modules/auth/auth.controller.ts
+++ user-service/src/modules/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginRequest, LoginResponse } from './auth';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('login')
+  login(@Body() loginRequest: LoginRequest): LoginResponse {
+    console.log('Login request received:', loginRequest);
+
+    return {
+      accessToken: 'mockAccessToken',
+      refreshToken: 'mockRefreshToken',
+    };
+  }
+}
