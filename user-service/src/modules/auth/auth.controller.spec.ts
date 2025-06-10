cdbff39b68e76c8cbf8163b86a332782533159d0
Joe Kim <joesm.genie@gmail.com>
2025-06-10 22:24:04 +0700

    inject auth service to auth controller

--- user-service/src/modules/auth/auth.controller.spec.ts
+++ user-service/src/modules/auth/auth.controller.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+const mockAuthService = {};
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
--- user-service/src/modules/auth/auth.controller.ts
+++ user-service/src/modules/auth/auth.controller.ts
@@ -1,8 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginRequest, LoginResponse } from './auth';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  private readonly authService: AuthService;
+
+  constructor(authService: AuthService) {
+    this.authService = authService;
+  }
+
   @Post('login')
   login(@Body() loginRequest: LoginRequest): LoginResponse {
     console.log('Login request received:', loginRequest);
