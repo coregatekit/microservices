50c0356395527a0cefbd515b03370005d52cae84
Joe Kim <joesm.genie@gmail.com>
2025-06-10 13:41:17 +0700

    fix module dependencies

--- user-service/src/db/db.module.ts
+++ user-service/src/db/db.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DrizzleAsyncProvider, drizzleProvider } from './db.provider';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [...drizzleProvider],
   exports: [DrizzleAsyncProvider],
 })
