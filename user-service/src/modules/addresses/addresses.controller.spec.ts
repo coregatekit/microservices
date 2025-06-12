1ab680867a4210566162fc0d444e5914e6b690ac
Joe Kim <joesm.genie@gmail.com>
2025-06-12 17:31:00 +0700

    feat: implement controller add new address

--- user-service/src/modules/addresses/addresses.controller.spec.ts
+++ user-service/src/modules/addresses/addresses.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AddressesController } from './addresses.controller';
+import { AddressesService } from './addresses.service';
+
+const mockAddressesService = {
+  addNewAddress: jest.fn(),
+};
 
 describe('AddressesController', () => {
   let controller: AddressesController;
@@ -7,6 +12,12 @@ describe('AddressesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AddressesController],
+      providers: [
+        {
+          provide: AddressesService,
+          useValue: mockAddressesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AddressesController>(AddressesController);
@@ -14,5 +25,33 @@ describe('AddressesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('addNewAddress', () => {
+    it('should call AddressesService.addNewAddress with correct data', async () => {
+      const addressData = {
+        userId: 'user123',
+        type: 'SHIPPING',
+        addressLine1: '123 Main St',
+        addressLine2: 'Apt 4B',
+        city: 'Springfield',
+        state: 'IL',
+        postalCode: '62701',
+        country: 'USA',
+        isDefault: true,
+      };
+      mockAddressesService.addNewAddress.mockResolvedValue({
+        id: 'FA831B00-7E34-4062-94BE-F4AB15F3FBE3',
+      });
+
+      const result = await controller.addNewAddress(addressData);
+
+      expect(mockAddressesService.addNewAddress).toHaveBeenCalledWith(
+        addressData,
+      );
+      expect(result).toEqual({
+        id: 'FA831B00-7E34-4062-94BE-F4AB15F3FBE3',
+      });
+    });
   });
 });
--- user-service/src/modules/addresses/addresses.controller.ts
+++ user-service/src/modules/addresses/addresses.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AddressesService } from './addresses.service';
+import { AddAddressDto } from './addresses';
 
 @Controller('addresses')
-export class AddressesController {}
+export class AddressesController {
+  private readonly addressesService: AddressesService;
+
+  constructor(addressesService: AddressesService) {
+    this.addressesService = addressesService;
+  }
+
+  @Post()
+  async addNewAddress(@Body() addressData: AddAddressDto) {
+    try {
+      return await this.addressesService.addNewAddress(addressData);
+    } catch (error) {
+      console.error('Error adding address:', error);
+      return {
+        message: 'Failed to add address',
+      };
+    }
+  }
+}
