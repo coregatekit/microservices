40f6b59f2bf5eebcb3ddbcfd2bf0520a50f63e3d
Joe Kim <joesm.genie@gmail.com>
2025-06-12 17:03:31 +0700

    feat: inject db to addresses service

--- user-service/src/modules/addresses/addresses.service.spec.ts
+++ user-service/src/modules/addresses/addresses.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AddressesService } from './addresses.service';
+import { DrizzleAsyncProvider } from '../../db/db.provider';
+
+const mockDb = {
+  insert: jest.fn(),
+  values: jest.fn(),
+  returning: jest.fn(),
+};
 
 describe('AddressesService', () => {
   let service: AddressesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AddressesService],
+      providers: [
+        AddressesService,
+        {
+          provide: DrizzleAsyncProvider,
+          useValue: mockDb,
+        },
+      ],
     }).compile();
 
     service = module.get<AddressesService>(AddressesService);
