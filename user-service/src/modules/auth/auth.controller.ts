b1cec27b92d0126f08aab842cbcc6d4e389357d7
Joe Kim <joesm.genie@gmail.com>
2025-06-12 21:03:04 +0700

    refactor: handle error route login

--- user-service/src/modules/auth/auth.controller.ts
+++ user-service/src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { LoginRequest, LoginResponse } from './auth';
 import { AuthService } from './auth.service';
 import { Public } from '../../decorators/public';
+import { HttpResponse } from 'src/common/http-response';
+import { ResultStatus } from 'src/common/enum/result';
 
 @Controller('auth')
 export class AuthController {
@@ -12,11 +14,28 @@ export class AuthController {
   }
 
   @Public()
+  @HttpCode(200)
   @Post('login')
-  async login(@Body() loginRequest: LoginRequest): Promise<LoginResponse> {
-    return await this.authService.login(
-      loginRequest.email,
-      loginRequest.password,
-    );
+  async login(
+    @Body() loginRequest: LoginRequest,
+  ): Promise<HttpResponse<LoginResponse>> {
+    try {
+      return {
+        status: ResultStatus.SUCCESS,
+        message: 'Login successful',
+        data: await this.authService.login(
+          loginRequest.email,
+          loginRequest.password,
+        ),
+      };
+    } catch (error) {
+      console.error('Error during login:', error);
+      const errorMessage =
+        error instanceof Error ? error.message : 'Loin failed';
+      return {
+        status: ResultStatus.ERROR,
+        message: errorMessage,
+      };
+    }
   }
 }
