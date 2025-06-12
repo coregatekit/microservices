70c199f0f182d01e9a7b25c672b1e2222c9ce59e
Joe Kim <joesm.genie@gmail.com>
2025-06-12 17:24:26 +0700

    fix: inject db module to addresses module

--- user-service/src/modules/addresses/addresses.module.ts
+++ user-service/src/modules/addresses/addresses.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AddressesController } from './addresses.controller';
 import { AddressesService } from './addresses.service';
+import { DbModule } from '../../db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [AddressesController],
-  providers: [AddressesService]
+  providers: [AddressesService],
 })
 export class AddressesModule {}
