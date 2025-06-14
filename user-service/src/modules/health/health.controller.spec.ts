c20da758c264bc774364ffd8fb1a1b461fa35afb
Joe Kim <joesm.genie@gmail.com>
2025-06-14 11:13:10 +0700

    feat: enhance HealthController to return detailed health status and update tests

--- user-service/src/modules/health/health.controller.spec.ts
+++ user-service/src/modules/health/health.controller.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { ConfigService } from '@nestjs/config';
 
 describe('HealthController', () => {
   let controller: HealthController;
+  const mockConfigService = {
+    get: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthController],
+      providers: [
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
@@ -17,6 +27,10 @@ describe('HealthController', () => {
   });
 
   it('should return "OK" on health check', () => {
-    expect(controller.healthCheck()).toBe('OK');
+    mockConfigService.get.mockReturnValueOnce('user-service');
+    mockConfigService.get.mockReturnValueOnce('1.0.0');
+    expect(controller.healthCheck()).toBe(
+      'Healthy user-service version: 1.0.0',
+    );
   });
 });
--- user-service/src/modules/health/health.controller.ts
+++ user-service/src/modules/health/health.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../../decorators/public';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('health')
 export class HealthController {
+  constructor(private readonly configService: ConfigService) {}
+
+  @Public()
   @Get()
   healthCheck(): string {
-    return 'OK';
+    return `Healthy ${this.configService.get<string>('APP_NAME')} version: ${this.configService.get<string>('APP_VERSION')}`;
   }
 }
