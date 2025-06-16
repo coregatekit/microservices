aa686a0c44a1059c30e8c3faf34c8c5ba1935953
Joe Kim <joesm.genie@gmail.com>
2025-06-16 14:52:35 +0700

    feat: config http module and inject to keycloak service

--- user-service/src/modules/keycloak/keycloak.module.ts
+++ user-service/src/modules/keycloak/keycloak.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { KeycloakService } from './keycloak.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000, // Set a timeout for HTTP requests
+      maxRedirects: 5, // Set the maximum number of redirects
+    }),
+  ],
   providers: [KeycloakService],
 })
 export class KeycloakModule {}
--- user-service/src/modules/keycloak/keycloak.service.ts
+++ user-service/src/modules/keycloak/keycloak.service.ts
@@ -1,6 +1,11 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class KeycloakService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private httpService: HttpService,
+    private configService: ConfigService,
+  ) {}
 }
