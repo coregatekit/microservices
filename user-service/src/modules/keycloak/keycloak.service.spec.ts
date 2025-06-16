dade14eb6dbefa0e58cc6e622daa8396a4b0cada
Joe Kim <joesm.genie@gmail.com>
2025-06-16 15:40:51 +0700

    feat: create method getAccessToken

--- user-service/src/modules/keycloak/keycloak.service.spec.ts
+++ user-service/src/modules/keycloak/keycloak.service.spec.ts
@@ -1,12 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Test, TestingModule } from '@nestjs/testing';
 import { KeycloakService } from './keycloak.service';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
 
 describe('KeycloakService', () => {
   let service: KeycloakService;
 
+  const mockConfigService = {
+    get: jest.fn().mockImplementation((key: string) => {
+      switch (key) {
+        case 'KEYCLOAK_BASE_URL':
+          return 'http://mock-keycloak-url';
+        case 'KEYCLOAK_REALM':
+          return 'mock-realm';
+        case 'KEYCLOAK_CLIENT_ID':
+          return 'mock-client-id';
+        case 'KEYCLOAK_CLIENT_SECRET':
+          return 'mock-client-secret';
+        default:
+          return null;
+      }
+    }),
+  };
+  const mockHttpService = {
+    post: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [KeycloakService],
+      providers: [
+        KeycloakService,
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
+        {
+          provide: HttpService,
+          useValue: mockHttpService,
+        },
+      ],
     }).compile();
 
     service = module.get<KeycloakService>(KeycloakService);
@@ -14,5 +49,27 @@ describe('KeycloakService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAccessToken', () => {
+    it('should return an access token', async () => {
+      mockHttpService.post.mockImplementation(() => {
+        const mockResponse: AxiosResponse = {
+          data: { access_token: 'mock_access_token' },
+          status: 200,
+          statusText: 'OK',
+          headers: {},
+          config: { headers: {} as any },
+        };
+        return of(mockResponse);
+      });
+      const result = await service.getAccessToken();
+
+      expect(mockHttpService.post).toHaveBeenCalledWith(
+        'http://mock-keycloak-url/realms/mock-realm/protocol/openid-connect/token',
+        expect.any(URLSearchParams),
+      );
+      expect(result).toEqual('mock_access_token');
+    });
   });
 });
--- user-service/src/modules/keycloak/keycloak.service.ts
+++ user-service/src/modules/keycloak/keycloak.service.ts
@@ -1,11 +1,42 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+import { AccessTokenResponse } from './keycloak.type';
 
 @Injectable()
 export class KeycloakService {
+  private readonly logger: Logger;
+
   constructor(
     private httpService: HttpService,
     private configService: ConfigService,
-  ) {}
+  ) {
+    this.logger = new Logger(KeycloakService.name);
+  }
+
+  async getAccessToken(): Promise<string> {
+    const url = `${this.configService.get<string>('KEYCLOAK_BASE_URL')}/realms/${this.configService.get<string>('KEYCLOAK_REALM')}/protocol/openid-connect/token`;
+    const params = new URLSearchParams();
+    params.append(
+      'client_id',
+      this.configService.get<string>('KEYCLOAK_CLIENT_ID') || '',
+    );
+    params.append(
+      'client_secret',
+      this.configService.get<string>('KEYCLOAK_CLIENT_SECRET') || '',
+    );
+    params.append('grant_type', 'client_credentials');
+
+    const { data } = await firstValueFrom(
+      this.httpService.post<AccessTokenResponse>(url, params).pipe(
+        catchError((error: AxiosError) => {
+          this.logger.error('Error fetching access token', error);
+          throw new Error('Failed to fetch access token from Keycloak');
+        }),
+      ),
+    );
+    return data.access_token;
+  }
 }
