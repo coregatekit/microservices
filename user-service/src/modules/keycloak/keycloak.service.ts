ee31d094233b1ff27ee930ee9f66a3d4260bbe20
Joe Kim <joesm.genie@gmail.com>
2025-06-16 14:45:01 +0700

    feat: inject config service to keycloak service

--- user-service/src/modules/keycloak/keycloak.service.ts
+++ user-service/src/modules/keycloak/keycloak.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class KeycloakService {}
+export class KeycloakService {
+  constructor(private configService: ConfigService) {}
+}
