10edc4071f97f4765377b0440c72b48195955504
Joe Kim <joesm.genie@gmail.com>
2025-06-23 14:34:21 +0700

    refactor: remove JwtModule import from AuthModule

--- user-service/src/modules/auth/auth.module.ts
+++ user-service/src/modules/auth/auth.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { KeycloakModule } from '../keycloak/keycloak.module';
 
 @Module({
-  imports: [
-    KeycloakModule,
-    ConfigModule,
-    JwtModule.register({
-      global: true,
-    }),
-  ],
+  imports: [KeycloakModule, ConfigModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
