ba49fc853a1897ae4dfdefcdbe2978ce1041a0a8
Joe Kim <joesm.genie@gmail.com>
2025-06-30 11:49:15 +0700

    feat: add Category class

---
+++ product-service/src/main/java/dev/coregate/product/api/entities/Category.java
@@ -0,0 +1,59 @@
+package dev.coregate.product.api.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "categories")
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id", updatable = false, nullable = false)
+  private UUID id;
+
+  @Column(name = "name", nullable = false, length = 255)
+  private String name;
+
+  @Column(name = "description", nullable = true, length = 500)
+  private String description;
+
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  protected void onCreate() {
+    LocalDateTime now = LocalDateTime.now();
+    this.createdAt = now;
+    this.updatedAt = now;
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  public Category() {
+  }
+
+  public Category(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+}
