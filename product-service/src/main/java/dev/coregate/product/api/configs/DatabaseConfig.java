75d7e6f6bc44134a7d11779f704d02d11b07ce44
Joe Kim <joesm.genie@gmail.com>
2025-06-30 13:03:42 +0700

    feat: add database configurations

---
+++ product-service/src/main/java/dev/coregate/product/api/configs/DatabaseConfig.java
@@ -0,0 +1,50 @@
+package dev.coregate.product.api.configs;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+  @Value("${spring.datasource.url}")
+  private String databaseUrl;
+
+  @Value("${spring.datasource.username}")
+  private String databaseUsername;
+
+  @Value("${spring.datasource.password}")
+  private String databasePassword;
+
+  @Bean
+  @Primary
+  public DataSource dataSource() {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(databaseUrl);
+    config.setUsername(databaseUsername);
+    config.setPassword(databasePassword);
+    config.setDriverClassName("org.postgresql.Driver");
+
+    // Connection pool settings
+    config.setMaximumPoolSize(20);
+    config.setMinimumIdle(10);
+    config.setConnectionTimeout(30000); // 30 seconds
+    config.setIdleTimeout(600000); // 10 minutes
+    config.setMaxLifetime(1800000); // 30 minutes
+    config.setAutoCommit(false); // Disable auto-commit for better transaction management
+    config.setPoolName("ProductServiceHikariConnectionPool");
+
+    // Performance settings
+    config.addDataSourceProperty("cachePrepStmts", "true");
+    config.addDataSourceProperty("prepStmtCacheSize", "250");
+    config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+    return new HikariDataSource(config);
+  }
+}
