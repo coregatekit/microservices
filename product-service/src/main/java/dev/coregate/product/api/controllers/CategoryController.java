a89d6c778d6c66ce1860136fc8e3b1be05d9078f
Joe Kim <joesm.genie@gmail.com>
2025-07-03 13:56:26 +0700

    fix: wrong import request body

--- product-service/src/main/java/dev/coregate/product/api/controllers/CategoryController.java
+++ product-service/src/main/java/dev/coregate/product/api/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package dev.coregate.product.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.coregate.product.api.dto.requests.CreateCategoryRequest;
 import dev.coregate.product.api.dto.responses.CategoryResponse;
 import dev.coregate.product.api.services.CategoryService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
