83b4284c9ed1ba17dc7fd186ae3d56a59f16e69b
Joe Kim <joesm.genie@gmail.com>
2025-07-04 10:59:07 +0700

    feat: add product service

---
+++ product-service/src/main/java/dev/coregate/product/api/services/ProductService.java
@@ -0,0 +1,5 @@
+package dev.coregate.product.api.services;
+
+public interface ProductService {
+  
+}
---
+++ product-service/src/main/java/dev/coregate/product/api/services/impl/ProductServiceImpl.java
@@ -0,0 +1,12 @@
+package dev.coregate.product.api.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import dev.coregate.product.api.services.ProductService;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements ProductService {
+  
+}
