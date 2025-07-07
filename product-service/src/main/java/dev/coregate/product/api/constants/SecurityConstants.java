b4805e314b2e74a1824bd525795da623cf46e7e8
Joe Kim <joesm.genie@gmail.com>
2025-07-07 17:28:51 +0700

    refactor: enhance documentation in SecurityConstants for clarity

--- product-service/src/main/java/dev/coregate/product/api/constants/SecurityConstants.java
+++ product-service/src/main/java/dev/coregate/product/api/constants/SecurityConstants.java
@@ -1,5 +1,10 @@
 package dev.coregate.product.api.constants;
 
+/**
+ * SecurityConstants provides constants for role-based access control.
+ * These constants are used to define security rules in the application.
+ */
+
 public final class SecurityConstants {
   public static final String HAS_MANAGER_ROLE = "hasRole('" + "MANAGER" + "')";
   public static final String HAS_USER_ROLE = "hasRole('" + "USER" + "')";
