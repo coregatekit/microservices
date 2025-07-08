ef6aac0e31ae8c2dd553024404c961a090620162
Joe Kim <joesm.genie@gmail.com>
2025-07-08 17:11:14 +0700

    test: create test file register user

--- testing/apitesting/tests/user-service/users/register-user.spec.ts
+++ testing/apitesting/tests/user-service/users/register-user.spec.ts
@@ -1,3 +1,23 @@
-import test from '@playwright/test';
+import test, { APIRequestContext, request } from '@playwright/test';
 
-test.describe('Register User', () => {});
+test.describe('Register User', () => {
+  let context: APIRequestContext;
+  const username = 'tester@coregate.dev';
+
+  test.beforeAll(async () => {
+    context = await request.newContext({
+      baseURL: 'http://localhost:9000',
+    });
+
+    await context.delete('/api/v1/testing/clear-user-data', {
+      data: { username: username },
+    });
+  });
+
+  test.afterAll(async () => {
+    await context.delete('/api/v1/testing/clear-user-data', {
+      data: { username: username },
+    });
+    await context.dispose();
+  });
+});
